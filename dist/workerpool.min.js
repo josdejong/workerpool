0e678522fa991648e6c15a2092afa687c0e02e5d
jos <wjosdejong@gmail.com>
2017-08-19 11:49:08 +0200

    Released v2.2.3

--- dist/workerpool.min.js
+++ dist/workerpool.min.js
@@ -4,8 +4,8 @@
  *
  * Offload tasks to a pool of workers on node.js and in the browser.
  *
- * @version 2.2.2
- * @date    2017-07-08
+ * @version 2.2.3
+ * @date    2017-08-19
  *
  * @license
  * Copyright (C) 2014-2016 Jos de Jong <wjosdejong@gmail.com>
