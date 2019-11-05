2377e7729f1f0fb71d2a5094d78a445c0733cb47
wudi <acgrid@gmail.com>
2019-11-05 09:15:12 +0800

    Test: simplify environment related test

--- src/environment.js
+++ src/environment.js
@@ -1,13 +1,14 @@
 var requireFoolWebpack = require('./requireFoolWebpack');
 
 // source: https://github.com/flexdinesh/browser-or-node
-module.exports.isNode = isNode = function (nodeProcess) {
+var isNode = function (nodeProcess) {
     return (
         typeof nodeProcess !== 'undefined' &&
         nodeProcess.versions != null &&
         nodeProcess.versions.node != null
     );
 }
+module.exports.isNode = isNode
 
 // determines the JavaScript platform: browser or node
 module.exports.platform = isNode(process)
--- test/environment.test.js
+++ test/environment.test.js
@@ -1,23 +1,13 @@
 const assert = require('assert')
 const isNode = require('../src/environment').isNode
 
-const test = (process, should) => should(isNode(process))
-
-const shouldBeBrowser = isNode => {
+describe('Environment Detection', function () {
     it('is platform assigned to be browser', function () {
-        assert.strictEqual(isNode, false)
+        assert.strictEqual(isNode(undefined), false)
+        assert.strictEqual(isNode({}), false)
+        assert.strictEqual(isNode({ versions: {} }), false)
     })
-}
-
-const shouldBeNode = isNode => {
     it('is platform assigned to be node', function () {
-        assert.strictEqual(isNode, true)
+        assert.strictEqual(isNode({ versions: { node: '10.0.0' } }), true)
     })
-}
-
-describe('should distinguish environment', function () {
-    test(undefined, shouldBeBrowser)
-    test({}, shouldBeBrowser)
-    test({ versions: {} }, shouldBeBrowser)
-    test({ versions: { node: '10.0.0' } }, shouldBeNode)
 })
