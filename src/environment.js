f66567b7945beb9160f73be83d38792ca2fddfdf
acgrid <acgrid@users.noreply.github.com>
2019-11-06 18:54:11 +0800

    Fix environment detection in browser (#106)
    * Fix environment detection in browser
    `typeof process.versions != null` will fail if `process` is a defined variable, for example, browser environment with improper webpack bundled
    
    * Make isNode a function to be more test-friendly
    
    * Test: simplify environment related test

--- src/environment.js
+++ src/environment.js
@@ -1,13 +1,17 @@
 var requireFoolWebpack = require('./requireFoolWebpack');
 
 // source: https://github.com/flexdinesh/browser-or-node
-var isNode = (
-    typeof process !== 'undefined' &&
-    typeof process.versions != null &&
-    process.versions.node != null);
+var isNode = function (nodeProcess) {
+    return (
+        typeof nodeProcess !== 'undefined' &&
+        nodeProcess.versions != null &&
+        nodeProcess.versions.node != null
+    );
+}
+module.exports.isNode = isNode
 
 // determines the JavaScript platform: browser or node
-module.exports.platform = isNode
+module.exports.platform = isNode(process)
     ? 'node'
     : 'browser';
 
