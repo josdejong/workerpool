7c1e099fd471ccf7426621fc9b632211e30200fe
joshLong145 <jlong2@ramapo.edu>
2024-12-12 21:50:06 -0500

    dev: refactor abort example to show implemented changes

--- examples/abort.js
+++ examples/abort.js
@@ -1,16 +1,21 @@
 const workerpool = require("..");
 
-var workerCount = 0;
-
 // create a worker pool
 const pool = workerpool.pool(__dirname + "/workers/cleanupAbort.js", {
   // maximum time to wait for worker to cleanup it's resources
   // on termination before forcefully stopping the worker
   workerTerminateTimeout: 1000,
-  onCreateWorker: (args) => {
+  onCreateWorker: function (args) {
     console.log("New worker created");
-    workerCount += 1;
-  }
+  },
+  onAbortResolution: function(args) {
+    console.log("abort operation concluded for task:", args.id);
+    console.log("is worker terminating", args.isTerminating);
+  },
+  onTerminateWorker: function() {
+    console.log("worker terminated");
+  },
+  maxWorkers: 1,
 });
 
 function add (a, b) {
@@ -18,29 +23,36 @@ function add (a, b) {
 }
 
 const main = async () => {
-    const cleanedUpTask = pool.exec('asyncTimeout', []).timeout(1_000).catch((err) => {
-        console.log("task timeout");
+    let abortResolverSuccess;
+    await pool.exec('asyncTimeout', [], {
+        onAbortStart: async function(args) {
+            console.log("abort operation started");
+            abortResolverSuccess = args.taskResolver.promise;
+        }
+    }).timeout(100).catch((err) => {
         console.log("timeout occured: ", err.message);
-        console.log("worker count ", workerCount);
-        return pool.exec(add, [1, 2]).then((sum) => {
-            console.log('add result', sum);
-            console.log("worker count: ", workerCount);
-        });
     });
-    await cleanedUpTask;
 
-    const canceledTask = pool.exec('asyncAbortHandlerNeverResolves').cancel().catch((err) => {
+    await abortResolverSuccess.catch((err) => {
+        console.log("", err);
+    });
+
+    console.log("pool status after abort operation:", pool.stats());
+    
+
+    let abortResolverFailure;
+    await pool.exec('asyncAbortHandlerNeverResolves', [], {
+        onAbortStart: function(args) {
+            console.log("abort operation started");
+            abortResolverFailure = args.taskResolver.promise;
+        }
+    }).cancel().catch((err) => {
         console.log("task canceled");
         console.log("cancel occured: ", err.message);
-        console.log("worker count ", workerCount);
-        return pool.exec(add, [1, 2]).then((sum) => {
-            console.log('add result', sum);
-            console.log("worker count: ", workerCount);
-        }); 
     });
 
-    await canceledTask;
+    console.log("final pool stats", pool.stats());
+    // we dont need to terminate the pool, since all workers should be terminated by this point even though there is a handler.
 }
-
 
 main();
